9c40a5524722b1bb3d7fb47a9c99e2a8ee4dbfac
maartentbm <M.H.Flikkema@student.tudelft.nl>
2013-11-22 10:29:33 +0100

    Functions

---
+++ src/Functions/Functions.java
@@ -0,0 +1,33 @@
+package Functions;
+
+/**
+ * Declaration of functions in a static enum.
+ * Parser will use this enum for the functions.
+ */
+enum Functions {
+	/*
+	 * Register your functions here!
+	 */
+	ADD(Add.class),
+	POWER(Power.class),
+	SUBTRACT(Subtract.class);
+	
+	/*
+	 *  Functions.valueOf("ADD") => ADD
+	 *  ADD.toString() => "ADD"
+	 */
+	
+	Class<? extends Function> c;
+	
+	Functions(Class <? extends Function > f)
+	{
+		c = f;
+	}
+	
+	Function create(Function... arguments) throws InstantiationException, IllegalAccessException
+	{
+		Function f = c.newInstance();
+		f.arguments = arguments;
+		return f;
+	}
+}
