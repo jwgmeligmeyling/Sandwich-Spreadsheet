d3d9fd9d851b64866194107003680f32a3637ae5
jwgmeligmeyling <j.gmeligmeyling@student.tudelft.nl>
2013-11-27 19:38:16 +0100

    Javadoc for interfaces

--- src/Interfaces/Cell.java
+++ src/Interfaces/Cell.java
@@ -1,21 +1,54 @@
 package Interfaces;
 
 import java.awt.Color;
+import java.util.Observer;
 
 /**
  * @author Maarten Flikkema
  * @author Liam Clark
+ * @author Jan-Willem Gmelig Meyling
  */
-public interface Cell {
+public interface Cell extends Observer {
 	
+	/**
+	 * Method to get the current value of the cell, for example "5"
+	 * @return value of this cell
+	 */
 	public String getValue();
+	
+	/**
+	 * Method to get the current input of the cell, for example "ADD(3,2)"
+	 * @return input of this cell
+	 */
 	public String getInput();
+	
+	/**
+	 * Method to change the current input of the cell
+	 * @param input to parse
+	 */
 	public void setInput(String newInput);
-	public void updateValue();
 
+	/**
+	 * Method to get the foreground-color
+	 * @return foreground-color for this cell
+	 */
 	public Color getFColor();
+	
+	/**
+	 * Method to get the background-color
+	 * @return backgroun-dcolor for this cell
+	 */
 	public Color getBColor();
 
+	/**
+	 * Set the foreground-color for this cell
+	 * @param foreground-color
+	 */
 	public void setFColor(Color newFColor);
+	
+	/**
+	 * Set the backgroud-color for this cell
+	 * @param backgroud-color
+	 */
 	public void setBColor(Color newBColor);
 }
--- src/Interfaces/Row.java
+++ src/Interfaces/Row.java
@@ -1,14 +1,23 @@
 package Interfaces;
 
+import File.Cell;
+
 /**
  * @author Maarten Flikkema
  * @author Liam Clark
  */
-import File.Cell;
-
 public interface Row {
-
+	
+	/**
+	 * Get the row at a specific column index
+	 * @param column index
+	 * @return row at index
+	 */
 	public Cell getCell(int colIndex);
 	
+	/*
+	 * Moet dit niet vanuit de andere kant bekeken worden?
+	 * Wellicht een overbodige functie.
+	 */
 	public int getRowNum();
 }
