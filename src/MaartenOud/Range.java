389fb0181cbc454d516d4d671fb5762cbc7cf174
maartentbm <M.H.Flikkema@student.tudelft.nl>
2013-12-09 23:59:44 +0100

    Bewaar mijn oude Model-zooi voor just a while + Re-do JavaDoc zonder Zandbak en MaartenOud

---
+++ src/MaartenOud/Range.java
@@ -0,0 +1,54 @@
+package MaartenOud;
+
+import java.util.ArrayList;
+import File.Cell;
+import File.Sheet;
+
+/*
+ * Verzoek om deze klasse te verplaatsen naar package "File" - Maarten
+ */
+
+/**
+ * This class can be used to define parts of sheets. One purpose for this is the
+ * so called user defined range, which makes it posible for users to refer to
+ * ranges with a name in stead of via cell references.
+ * 
+ * @author Maarten Flikkema
+ * 
+ */
+public class Range implements Interfaces.Range {
+
+	private ArrayList<Cell> cells;
+	private String name;
+
+	public Range(Sheet sheet, Cell upLeft, Cell downRight) {
+		cells = sheet.getRangeCells(upLeft, downRight);
+	}
+
+	public Range(Sheet sheet, int rowUp, int colLeft, int rowDown, int colRight) {
+		cells = sheet.getRangeCells(rowUp, colLeft, rowDown, colRight);
+	}
+
+	public void setName(String newName) {
+		name = newName;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	@Override
+	public void setCells(Sheet sheet, Cell upLeft, Cell downRight) {
+		cells = sheet.getRangeCells(upLeft, downRight);
+	}
+
+	@Override
+	public void setCells(Sheet sheet, int rowUp, int colLeft, int rowDown, int colRight) {
+		cells = sheet.getRangeCells(rowUp, colLeft, rowDown, colRight);
+	}
+
+	@Override
+	public ArrayList<Cell> getCells() {
+		return cells;
+	}
+}
