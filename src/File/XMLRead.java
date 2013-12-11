fa9a3f0a9ba1b74b5f50fc95181a838885c01f23
Jim <Jim@145.94.195.228>
2013-12-11 15:16:19 +0100

    XMLRead verplaatst en een begin van de TestRead

--- src/File/XMLRead.java
+++ src/File/XMLRead.java
@@ -1,3 +1,5 @@
+package File;
+import File.*;
 import java.io.IOException;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -16,7 +18,7 @@ import org.xml.sax.helpers.DefaultHandler;
  */
 public class XMLRead {
 	
-	public static void main(String[] args) {
+	public static Sheet read(String path) {
 		
 		try {
 			/**
@@ -27,6 +29,9 @@ public class XMLRead {
 			 */
 			SAXParserFactory factory = SAXParserFactory.newInstance();
 			SAXParser saxParser = factory.newSAXParser();
+			
+			final Sheet sheet = new Sheet();
+			final String[] attribvalues = new String[3];
 			
 			final StringBuilder inhoud = new StringBuilder();
 			
@@ -44,24 +49,33 @@ public class XMLRead {
 					System.out.println("Start Element:  " + qName);
 
 					int length = attributes.getLength();
-
+					
+					//Indien "type" leeg is
+					if(length < 3){
+						attribvalues[2] = "";
+					}
+					
+					//De attributen ophalen en in de final array attribvalues plaatsen
 					for (int i = 0; i < length; i++) {
 						String attrname = attributes.getQName(i);
 						System.out.println("Attr. Name:     " + attrname);
 						String value = attributes.getValue(i);
 						System.out.println("Value:          " + value);
+						attribvalues[i] = value;
 					}
 					
+					//Als het element een CELL is, zet de boolean op true en laat inlezen
 					if (qName.equalsIgnoreCase("CELL")) {
 						bcell = true;
+						//sheet.createCell("",Integer.parseInt(attribvalues[0]),Integer.parseInt(attribvalues[1]));
 					}
 				}
 
 				/**
 				 * Deze functie print de inhoud van het element. De String
 				 * inhoud neemt de opgegeven array van chars, en vormt zo de
-				 * string met het antwoord. Dit dienen wij te gebruiken om
-				 * verder te gaan.
+				 * string met het antwoord. De Stringbuilder neemt al deze onderdelen
+				 * en maakt zo de inhoud.
 				 */
 				public void characters(char ch[], int start, int length) throws SAXException {
 					if (bcell) {
@@ -72,11 +86,12 @@ public class XMLRead {
 				}
 				
 				/**
-				 * Print het einde van het element.
+				 * Print het einde van het element en print de uiteindelijke String.
 				 */
 				public void endElement(String uri, String localName, String qName) throws SAXException {
 					if(qName.equalsIgnoreCase("CELL")){
 						bcell = false;
+						sheet.createCell(inhoud.toString(),Integer.parseInt(attribvalues[1]),Integer.parseInt(attribvalues[0]));
 						System.out.println("Final Cell:     "+inhoud.toString());
 					}
 					
@@ -86,7 +101,9 @@ public class XMLRead {
 				}
 			};
 
-			saxParser.parse("xml/output.xml", handler);
+			saxParser.parse(path, handler);
+			
+			return sheet;
 
 		} catch (SAXException e) {
 			System.out.println("Unhandled SAXException");
@@ -95,5 +112,6 @@ public class XMLRead {
 		} catch (IOException e) {
 			System.out.println("Unhandled IOException");
 		}
+		return null;
 	}
 }
