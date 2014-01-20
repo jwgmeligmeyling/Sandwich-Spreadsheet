e37e46f129cbebe60fb3147a37122c96c10e8ba7
LiamClark <LC@10.0.1.9>
2014-01-20 22:57:10 +0100

    updates to formulepicker

--- src/GUI/SFormulePicker.java
+++ src/GUI/SFormulePicker.java
@@ -6,48 +6,79 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 import Parser.Function;
 
 @SuppressWarnings("serial")
-public class SFormulePicker extends JDialog implements ActionListener {
+public class SFormulePicker extends JDialog implements ActionListener, ListSelectionListener {
 	// Implements action listener voor de clicks in je list
 	private final JList lijst;
 	private final Window window;
+	private JLabel description;
 
 	// constructor
 	public SFormulePicker(Window window) {
 		super(window, "Formules", true);
 		this.window = window;
-		setSize(300, 200);
+		setSize(450, 200);
 		lijst = new JList(Function.values());
 
 		lijst.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
 		lijst.setLayoutOrientation(JList.HORIZONTAL_WRAP);
 		JScrollPane listScroller = new JScrollPane(lijst);
-		listScroller.setPreferredSize(new Dimension(300, 200));
+		listScroller.setPreferredSize(new Dimension(300, 300));
 		lijst.setVisibleRowCount(-1);
+		lijst.addListSelectionListener(this);
+		
 		Container container = getContentPane();
 		container.setLayout(new BorderLayout());
 
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setLayout(new FlowLayout());
-		setLayout(new BorderLayout());
-		JButton cancelButton = new JButton("Cancel");
-		cancelButton.addActionListener(this);
-		buttonPanel.add(cancelButton);
+		//setLayout(new BorderLayout());
+		JButton okButton = new JButton("Ok");
+		okButton.addActionListener(this);
+		buttonPanel.add(okButton);
+		
+		
 
 		setLayout(new BorderLayout());
+		JPanel top= new JPanel();
+		JPanel bot= new JPanel();
+		description=new JLabel();
+		bot.setLayout(new BorderLayout());
+		bot.add(buttonPanel, BorderLayout.CENTER);
+		bot.add(description, BorderLayout.PAGE_START);
+		JLabel tekst=new JLabel("Kies je formule.");
+		String[] types={"math","logical"}; // more to be added
+		JComboBox functies= new JComboBox(types);
+		top.add(tekst);
+		top.add(functies);
+		container.add(top,BorderLayout.PAGE_START);
 		container.add(listScroller, BorderLayout.CENTER);
-		container.add(buttonPanel, BorderLayout.PAGE_END);
+		container.add(bot, BorderLayout.PAGE_END);
+		
+	}
+	@Override
+	public void valueChanged(ListSelectionEvent arg0) {
+		Function selected=(Function)lijst.getSelectedValue();
+		System.out.println(selected);
+		description.setText(selected.getDescription());
 	}
 
 	@Override
@@ -64,8 +95,16 @@ public class SFormulePicker extends JDialog implements ActionListener {
 				}
 				veld.setText(currentInput + functie.toString() + "()");
 			}
+			
 			dispose();
 		}
 
 	}
+	
+	/*public List<Function> sort(){
+		List<Function> functieSelect = Arrays.asList(Function.values());
+		Collections.sort(functieSelect,new Comparator<Fuction>()){	
+			if()
+		}
+	}*/
 }
