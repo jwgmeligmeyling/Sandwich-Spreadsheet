af67f651e217c63e1546b5bb4719f534b5f95182
maartentbm <M.H.Flikkema@student.tudelft.nl>
2014-01-22 21:53:27 +0100

    Verbetering SFormulaPicker

--- src/GUI/SFormulePicker.java
+++ src/GUI/SFormulePicker.java
@@ -3,7 +3,6 @@ package GUI;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -29,8 +28,7 @@ import javax.swing.event.ListSelectionListener;
 import Parser.Function;
 
 @SuppressWarnings("serial")
-public class SFormulePicker extends JDialog implements ActionListener,
-		ListSelectionListener {
+public class SFormulePicker extends JDialog implements ActionListener, ListSelectionListener {
 
 	// Implements action listener voor de clicks in je list
 	private final Window window;
@@ -53,7 +51,7 @@ public class SFormulePicker extends JDialog implements ActionListener,
 		
 		window = windowIn;
 		setResizable(false);
-		setLayout(new BorderLayout(10, 10));
+		setLayout(new BorderLayout(7, 4));
 
 		JLabel emptyLeft = new JLabel();
 		JLabel emptyRight = new JLabel();
@@ -64,11 +62,12 @@ public class SFormulePicker extends JDialog implements ActionListener,
 			JPanel centerBox = new JPanel(new BorderLayout());
 				JPanel center = new JPanel(new BorderLayout());
 			JPanel bottom = new JPanel(new BorderLayout());
-				JPanel info = new JPanel(new BorderLayout());
+				JPanel infoBox = new JPanel(new BorderLayout());	
+					JPanel info = new JPanel(new BorderLayout());
 				JPanel buttonPanel = new JPanel(new FlowLayout());
 		
 		lijst = new JList(getSortedFunctions());
-		lijst.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+		lijst.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		lijst.setLayoutOrientation(JList.VERTICAL);
 		JScrollPane listScroller = new JScrollPane(lijst);
 		lijst.setVisibleRowCount(-1);
@@ -80,35 +79,39 @@ public class SFormulePicker extends JDialog implements ActionListener,
 				}
 			}
 		});
-		
+
 		centerBox.add(center, BorderLayout.CENTER);
 		centerBox.setBorder(BorderFactory.createTitledBorder("Choose a function to insert:"));
-		center.setBorder(new EmptyBorder(10, 10, 10, 10) );
+		center.setBorder(new EmptyBorder(10, 10, 10, 10));
 		center.add(listScroller, BorderLayout.CENTER);
+
+		infoBox.add(info, BorderLayout.CENTER);
+		infoBox.setBorder(BorderFactory.createTitledBorder("Description"));
+		arguments = new JLabel();
+		description = new JLabel();
+		
+		info.setBorder(new EmptyBorder(2, 5, 5, 2));
+		info.add(arguments, BorderLayout.PAGE_START);
+		info.add(description, BorderLayout.CENTER);
+		infoBox.setPreferredSize(new Dimension(300, 100));
 
 		JButton okButton = new JButton("Insert");
 		okButton.addActionListener(this);
 		buttonPanel.add(okButton);
 
-		description = new JLabel("Description: ");
-		arguments = new JLabel("Expected arguments: ");
-		info.add(description, BorderLayout.PAGE_START);
-		info.add(arguments, BorderLayout.PAGE_END);
-
-		bottom.setPreferredSize(new Dimension(300, 100));
-		bottom.add(info, BorderLayout.PAGE_START);
+		bottom.add(infoBox, BorderLayout.PAGE_START);
 		bottom.add(buttonPanel, BorderLayout.PAGE_END);
 
 		container.add(centerBox, BorderLayout.CENTER);
 		container.add(bottom, BorderLayout.PAGE_END);
 
+		add(container, BorderLayout.CENTER);
 		add(emptyLeft, BorderLayout.WEST);
 		add(emptyTop, BorderLayout.PAGE_START);
 		add(emptyRight, BorderLayout.EAST);
 		add(emptyBottom, BorderLayout.PAGE_END);
-		add(container, BorderLayout.CENTER);
 
-		pack();
+		//pack();
 		setSize(DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT);
 		setLocationRelativeTo(null);
 		setVisible(true);
@@ -117,8 +120,8 @@ public class SFormulePicker extends JDialog implements ActionListener,
 	@Override
 	public void valueChanged(ListSelectionEvent arg0) {
 		Function selected = (Function) lijst.getSelectedValue();
-		description.setText("<html>Description: " + selected.getDescription() + "</html>");
-		arguments.setText("<html>Expected arguments: " + selected.getArgumentList() + "</html>");
+		description.setText("<html><b></b>" + selected.getDescription() + "</html>");
+		arguments.setText("<html><b>Expected arguments:</b> " + selected.getArgumentList() + "</html>");
 	}
 
 	@Override
